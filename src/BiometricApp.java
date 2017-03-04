4f5a96fadc94585639417ced8798918a2ade411d
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-03-05 01:07:09 +0530

    Fix navigation mixed up bug

--- src/BiometricApp.java
+++ src/BiometricApp.java
@@ -9,16 +9,14 @@ public class BiometricApp {
         p.log("2) Register as a new user");
         p.log("-----------------------------------------");
         String userInput = p.userInputString();
-        if (userInput.equals("1")) {
+        if (userInput.equals("2")) {
             HandProfile newProfile = core.collectHandData(true);
             p.writeLineToFile(core.prepareToSave(newProfile));
-        } else if (userInput.equals("2")) {
+        } else if (userInput.equals("1")) {
             HandProfile existingProfile = core.collectHandData(false);
             System.out.println(core.searchProfile(existingProfile));
         } else {
             p.log("Invalid Input, Exiting.");
         }
     }
-
-
 }
